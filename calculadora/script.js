ca4c0ebcc72832670f7748a0905f053d3c60686c
Alessandro Rauschkolb <alessandroaleyiy@gmail.com>
2026-03-17 17:17:23 -0300

    Correcao de bugs

--- calculadora/script.js
+++ calculadora/script.js
@@ -2,13 +2,14 @@
 const display = document.getElementById('display');
 let calculando = document.getElementById('calculando');
 
+
 function appendtbackspace() {
-    
+
     display.value = display.value.slice(0, -1);
 }
 
 function appendToDisplay(value) {
-   
+
     display.value += value;
 }
 
@@ -17,16 +18,23 @@ function clearDisplay() {
 }
 
 function calculate() {
+
     try {
         calculado = true;
         display.value = parseFloat(calculando.textContent);
-        result = eval(variavel);
+        var result = eval(variavel);
         display.value = result;
+        calculando.textContent = variavel;
         variavel = result;
-        calculando.textContent = "";
     } catch (error) {
         display.value = 'Deu erro. ';
         console.error('Erro ao calcular:', error);
     }
+}
+function plus(variavel) {
+    console.log(variavel);
+    document.getElementById('calculando').textContent = variavel;
+    clearDisplay();
+
 }
 
