e804c1aa82bc848e97873a185f45a17475ae28f4
AlessandroRausch <alessandroaleyiy@gmail.com>
2026-03-17 22:11:39 -0300

    Melhorando UX

--- calculadora/script.js
+++ calculadora/script.js
@@ -17,24 +17,39 @@ function clearDisplay() {
     display.value = '';
 }
 
+function clear() {
+    variavel = "";
+    conta = [];
+    operador = [];
+    calculando.textContent = "";
+}
+
 function calculate() {
 
     try {
+        console.log(variavel);
         calculado = true;
         display.value = parseFloat(calculando.textContent);
         var result = eval(variavel);
         display.value = result;
         calculando.textContent = variavel;
         variavel = result;
+        while (operador.length > 0) {
+            while (conta.length > 0) {
+                conta.pop();
+            }
+            operador.pop();
+        }
+        
+        
     } catch (error) {
         display.value = 'Deu erro. ';
         console.error('Erro ao calcular:', error);
     }
 }
 function plus(variavel) {
-    console.log(variavel);
-    document.getElementById('calculando').textContent = variavel;
-    clearDisplay();
-
+   calculando.textContent = variavel.toString(), operador[operador.length - 1].toString();
+   conta.push(variavel.slice(0, -1));
+    variavel = '';
 }
 
